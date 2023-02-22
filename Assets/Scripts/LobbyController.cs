e3f91af9ab0c290034e91851c114cab8a7431265
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-02-22 22:22:46 +0530

    Added quit button

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,11 +8,22 @@ using UnityEngine.UI;
 public class LobbyController : MonoBehaviour
 {
     public Button buttonPlay;
+    public Button quitPlay;
 
     private void Awake()
     {
         buttonPlay.onClick.AddListener(StartGame);
+        quitPlay.onClick.AddListener(QuitGame);
     }
+
+    private void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
     private void StartGame()
     {
         SceneManager.LoadScene(1);
