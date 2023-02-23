10a020994cf9456134b9e4ead351261b4d7f254c
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-02-23 15:46:37 +0530

    Remvoved namespace

--- Assets/Scripts/Level/LevelEnd.cs
+++ Assets/Scripts/Level/LevelEnd.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static Levels;
 using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
--- Assets/Scripts/Level/SceneLoader.cs
+++ Assets/Scripts/Level/SceneLoader.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using static Levels;
 
 [RequireComponent(typeof(Button))]
 public class SceneLoader : MonoBehaviour
